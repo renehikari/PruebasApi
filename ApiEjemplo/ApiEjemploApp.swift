e33c12ad5ffa094f4b59b297f042537179942379
Rene Iest <rene.soto01@iest.edu.mx>
2025-10-24 10:32:05 -0600

    inicio

---
+++ ApiEjemplo/ApiEjemploApp.swift
@@ -0,0 +1,17 @@
+//
+//  ApiEjemploApp.swift
+//  ApiEjemplo
+//
+//  Created by rene on 24/10/25.
+//
+
+import SwiftUI
+
+@main
+struct ApiEjemploApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
